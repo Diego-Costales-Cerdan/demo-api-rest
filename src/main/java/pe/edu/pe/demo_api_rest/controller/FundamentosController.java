5a01aa9b9615f5f7119327582f0b7da972570817
Diego-Costales-Cerdán <pt73869118@idat.pe>
2025-02-20 10:55:52 -0500

    ejercicio terminados

--- src/main/java/pe/edu/pe/demo_api_rest/controller/FundamentosController.java
+++ src/main/java/pe/edu/pe/demo_api_rest/controller/FundamentosController.java
@@ -1,13 +1,24 @@
 package pe.edu.pe.demo_api_rest.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.pe.demo_api_rest.model.APIResponse;
+import pe.edu.pe.demo_api_rest.service.FundamentosService;
+import pe.edu.pe.demo_api_rest.service.IFundamentosService;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/fundamentos")
 public class FundamentosController {
+
+    private final IFundamentosService ifundamentosService;
 
 
     @GetMapping("/ejercicios1")
@@ -15,5 +26,40 @@ public class FundamentosController {
         return "Hola mundo Rest";
     }
 
+    @GetMapping("/primo")
+    public ResponseEntity<APIResponse<String>> ValidarPrimo(
+            @RequestParam int numero) {
+        String respuesta = ifundamentosService.validarNumeroPrimo(numero);
+
+
+        return ResponseEntity.ok(APIResponse.<String>builder()
+                .data(respuesta)
+                .message("ejercicio Correctamente")
+                .build());
+    }
+    @GetMapping("/factorial")
+    public ResponseEntity<APIResponse<String>> factorial(
+            @RequestParam int numero) {
+        String respuesta = ifundamentosService.calcularFactorial(numero);
+
+
+        return ResponseEntity.ok(APIResponse.<String>builder()
+                .data(respuesta)
+                .message("ejercicio Correctamente")
+                .build());
+    }
+    @GetMapping("/fibonacci")
+    public ResponseEntity<APIResponse<List<Integer>>> fibonacci(
+            @RequestParam int numero) {
+        List<Integer> respuesta = ifundamentosService.calcularFibonacci(numero);
+
+
+        return ResponseEntity.ok(APIResponse.<List<Integer>>builder()
+                .data(respuesta)
+                .message("ejercicio Correctamente")
+                .build());
+    }
 
 }
+
+
