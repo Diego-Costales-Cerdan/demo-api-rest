6a389d2f7699ae77eb52e4ffb47178d5f1cb5385
Diego-Costales-Cerdán <pt73869118@idat.pe>
2025-02-20 09:09:20 -0500

    listo

--- src/main/java/pe/edu/pe/demo_api_rest/controller/FundamentosController.java
+++ src/main/java/pe/edu/pe/demo_api_rest/controller/FundamentosController.java
@@ -1,10 +1,17 @@
 package pe.edu.pe.demo_api_rest.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/fundamentos")
 public class FundamentosController {
+
+
+    @GetMapping("/ejercicios1")
+    public String ejercicios1() {
+        return "Hola mundo Rest";
+    }
 }
